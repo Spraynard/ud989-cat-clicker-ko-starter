1eda5a15fa64c4d5b3287c12436338602cb99e85
Kellan Martin <kellan.martin@gmail.com>
2017-03-15 12:17:50 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 var viewModel = function() {
+
+	this.currentCat = ko.observable( new Cat() );
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+}
+
+var Cat = function () {
+
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://ww.fickr.com');
-
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
+	this.nickNames = ko.observableArray(["Tabitha", "Jangus", "Rangus", "Dangus", "The Crushinator"]);
 
 	this.powerLevel = ko.computed(function () {
 		if (this.clickCount() < 50) {
@@ -26,5 +33,4 @@ var viewModel = function() {
 		}
 	},this)
 }
-
 ko.applyBindings(new viewModel());
